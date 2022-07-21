83e0b14130cc8d92759fbcb97fff257544b8c9c7
Юлия Ермолаева <iuliiaermolaeva@MacBook-Pro-Ulia.local>
2022-07-21 21:32:31 +0300

    Реализуем сервер чата

---
+++ server/Server.java
@@ -0,0 +1,29 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Scanner;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(9445);
+            System.out.println("Сервер запущен!");
+            serverSocket.accept();
+            System.out.println("Клиент подключился");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+//        Scanner scanner = new Scanner(System.in);
+//        while(true){
+//            try{
+//                int a = scanner.nextInt();
+//                int b = scanner.nextInt();
+//                System.out.println(a/b);
+//            } catch (Exception e){
+//                System.out.println("Неверное значение");
+//            }
+//        }
+
